5d09effa19d283c1bc4fb2ab25288cf462ee67f1
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-08-09 08:18:36 -0400

    Completed Part 5, added synchronization to avoid overdrawing account.

--- src/main/java/com/github/perschola/AccountRunnable.java
+++ src/main/java/com/github/perschola/AccountRunnable.java
@@ -5,15 +5,21 @@ public class AccountRunnable implements Runnable {
     @Override
     public void run() {
         Account account = new Account();
-        while(account.getBalance() > 0) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() + " is withdrawing $10.");
-            account.withdraw(10D);
-            System.out.println("The remaining is balance is: " + account.getBalance());
+
+            for (int i=0; i < 5; i++)  {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (Account.class) {
+                    if (account.getBalance() >= 10) {
+                        System.out.println(Thread.currentThread().getName() + " is withdrawing $10.");
+                        account.withdraw(10D);
+                        System.out.println("The remaining balance is: " + account.getBalance());
+
+                    }
+                }
         }
     }
 }
