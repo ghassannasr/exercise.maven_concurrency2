7280f46dd58cf10c32000ed3e085127fde063552
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-08-09 20:31:36 -0400

    Implemented class Waiter (Part 7). No code executed yet.

--- src/main/java/com/github/perschola/Waiter.java
+++ src/main/java/com/github/perschola/Waiter.java
@@ -1,8 +1,28 @@
 package com.github.perschola;
 
+
+import java.util.Date;
+
 public class Waiter implements Runnable {
+
+    private Message msg;
+
+    public Waiter(Message message) {
+        this.msg = message;
+    }
+
     @Override
     public void run() {
-
+        String threadName = Thread.currentThread().getName();
+        synchronized (msg) {
+            System.out.println("Thread " + threadName + " waiting to get notified at time " + new Date().getTime());
+            try {
+                msg.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(threadName + " waiter thread got notified at time " + new Date().getTime());
+            System.out.println(threadName + " processed: " + msg.getMessage());
+        }
     }
 }
